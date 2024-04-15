a6a222c376b8b7f7fa5b61975ff34769b7ab7edc
Alessio Miaschi <38506673+alemiaschi@users.noreply.github.com>
2024-04-15 14:27:23 +0200

    Delete esercizi/lezione_1 directory

--- esercizi/lezione_1/es_3.py
+++
@@ -1,8 +0,0 @@
-'''Scrivere un codice in python che calcola il numero delle ore corrispondenti 
-all’età di una persona'''
-
-eta = input("Inserisci età: ")
-eta = int(eta)
-
-risultato = 365*24*eta
-print("La tua età in ore è: ", risultato)
