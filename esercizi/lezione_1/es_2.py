aebb2f413a675d465b64a3d38fe9ce9fb7ee75ee
Alessio Miaschi <38506673+alemiaschi@users.noreply.github.com>
2025-04-28 16:23:43 +0200

    Delete esercizi/lezione_1 directory

--- esercizi/lezione_1/es_2.py
+++
@@ -1,8 +0,0 @@
-'''Chiedere all’utente di inserire un numero intero e verificare 
-se tale numero è pari'''
-
-numero = input("Inserisci un numero: ")
-numero = int(numero)
-
-risultato = numero%2
-print(risultato == 0)
--- esercizi/lezione_1/es_5.py
+++
@@ -1,8 +0,0 @@
-'''Chiedere all’utente di inserire una stringa e crearne una nuova 
-invertendo il primo carattere con l’ultimo'''
-
-stringa = input("Inserisci una stringa: ")
-
-nuova_stringa = stringa[-1] + stringa[1:-1] + stringa[0]
-
-print("La nuova stringa è: ", nuova_stringa)
