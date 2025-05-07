5f5652f8f17fa91bf6e7adf07a39f2f5881975ff
Alessio Miaschi <38506673+alemiaschi@users.noreply.github.com>
2025-05-07 11:43:40 +0200

    Add files via upload

---
+++ esercizi/lezione_1/es_1.py
@@ -0,0 +1,11 @@
+''' Chiedere all’utente di inserire due numeri reali (tipo float) 
+x e y e stampare il valore di (x+y)/(x-y)'''
+
+x = input("Inserisci il primo numero: ")
+y = input("Inserisci il secondo numero: ")
+
+x = float(x)
+y = float(y)
+
+risultato = (x+y)/(x-y)
+print("Il risultato è: ", risultato)2+2: command not found
---
+++ esercizi/lezione_1/es_3.py
@@ -0,0 +1,8 @@
+'''Scrivere un codice in python che calcola il numero delle ore corrispondenti 
+all’età di una persona'''
+
+eta = input("Inserisci età: ")
+eta = int(eta)
+
+risultato = 365*24*eta
+print("La tua età in ore è: ", risultato)
